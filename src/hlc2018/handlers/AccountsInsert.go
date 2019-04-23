b56d7964f28c32a8bf096cf2256450237202fc1f
314@live.jp <__math>
2019-04-23 22:36:32 +0900

    load ammos

---
+++ src/hlc2018/handlers/AccountsInsert.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "github.com/labstack/echo"
+
+func AccountsInsertHandler(c echo.Context) error {
+	return nil
+}
--- src/hlc2018/tester/testLoader.go
+++ src/hlc2018/tester/testLoader.go
@@ -72,8 +72,115 @@ func LoadGetPhase(fileName string) []*testCase {
 	return ret
 }
 
-func RunTest() {
-	testCases := LoadGetPhase("./testdata/answers/phase_1_get.answ")
+type ammo struct {
+	id   int
+	url  *url.URL
+	json string
+}
+
+func LoadAmmo(ammoFile string) []*ammo {
+	fp, err := os.Open(ammoFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+
+	var ret []*ammo
+	scanner := bufio.NewScanner(fp)
+	for {
+		var lines []string
+		for i := 0; i < 10; i++ {
+			if !scanner.Scan() {
+				return ret
+			}
+			text := scanner.Text()
+			lines = append(lines, text)
+			if text == "" {
+				break
+			}
+		}
+		if lines[len(lines)-3] == "Content-Length: 0" {
+			lines = append(lines, "")
+		} else {
+			scanner.Scan()
+			lines = append(lines, scanner.Text())
+		}
+
+		tmp := strings.Split(lines[1], " ")
+		url, err := url.Parse(tmp[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := strconv.Atoi(url.Query().Get("query_id"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		a := &ammo{id, url, lines[len(lines)-1]}
+		ret = append(ret, a)
+	}
+	return ret
+}
+
+type answ struct {
+	id     int
+	status int
+}
+
+func LoadAnsw(answFile string) []*answ {
+	fp, err := os.Open(answFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+
+	var ret []*answ
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		line := scanner.Text()
+		tmp := strings.Split(line, "\t")
+		url, err := url.Parse(tmp[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		status, err := strconv.Atoi(tmp[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		id, err := strconv.Atoi(url.Query().Get("query_id"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ret = append(ret, &answ{id, status})
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return ret
+}
+
+func LoadPostPhase(ammoFile string, answFile string) []*testCase {
+	ammos := LoadAmmo(ammoFile)
+	answs := LoadAnsw(answFile)
+	mp := map[int]*ammo{}
+	for _, ammo := range ammos {
+		mp[ammo.id] = ammo
+	}
+
+	var cases []*testCase
+	for _, answ := range answs {
+		ammo := mp[answ.id]
+		cases = append(cases, &testCase{ammo.url, answ.status, ammo.json})
+	}
+
+	return cases
+}
+
+func RunTestCases(testCases []*testCase) {
 	for _, testCase := range testCases {
 		var routed bool
 		for _, route := range testRouters {
@@ -98,4 +205,13 @@ func RunTest() {
 			}
 		}
 	}
+}
+
+func RunTest() {
+	//testCases1 := LoadGetPhase("./testdata/answers/phase_1_get.answ")
+	//RunTestCases(testCases1)
+	testCases2 := LoadPostPhase("./testdata/ammo/phase_2_post.ammo", "./testdata/answers/phase_2_post.answ")
+	RunTestCases(testCases2)
+	testCases3 := LoadGetPhase("./testdata/answers/phase_3_get.answ")
+	RunTestCases(testCases3)
 }
